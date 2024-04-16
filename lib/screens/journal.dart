2d7ed75d5c877c55f4c04a28f7034b790dd4fedd
FelixDigitalis <64135206+FelixDigitalis@users.noreply.github.com>
2024-04-16 09:44:11 +0200

    Basic Settings Menu

--- lib/screens/journal.dart
+++ lib/screens/journal.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'settings.dart';
 
 class Journal extends StatefulWidget {
   const Journal({super.key});
@@ -27,6 +27,28 @@ class _JournalState extends State<Journal> {
       appBar: AppBar(
         title: const Text('SproutJournal 🌱'),
         backgroundColor: Colors.green,
+        // Settings Menu
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: (String result) {
+              switch (result) {
+                case 'settings':
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SettingsMenu()),
+                  );
+                  break;
+              }
+            },
+            // Settings Menu Pop up menu
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+              const PopupMenuItem<String>(
+                value: 'settings',
+                child: Text('Settings'),
+              ),
+            ],
+          ),
+        ],
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
---
+++ lib/screens/settings.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+
+class SettingsMenu extends StatefulWidget {
+  //const SettingsMenu({super.key});
+
+  @override
+  State<SettingsMenu> createState() => _SettingsMenuState();
+}
+
+class _SettingsMenuState extends State<SettingsMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Einstellungen 🌱'),
+        backgroundColor: Colors.green,
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('Über'),
+            subtitle: const Text(
+                'SproutJournal, ein App Projekt von Felix im Sommersemester 2024'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
