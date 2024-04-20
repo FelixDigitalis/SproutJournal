25b76e92d02b51dc091bc897a41c52aad37c6e2a
FelixDigitalis <64135206+FelixDigitalis@users.noreply.github.com>
2024-04-20 15:52:11 +0200

    logger

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:logger/logger.dart';
 
 void main() {
+  Logger logger = Logger(
+    printer: PrettyPrinter(),
+  );
+
+  // Logger loggerNoStack = Logger(
+  //   printer: PrettyPrinter(methodCount: 0),
+  // );
+
+  logger.i("Main started");
+
   runApp(const App());
 }
-
-
-
-
-
